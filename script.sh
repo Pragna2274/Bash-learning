42c4b699289d4b7264a915553c23f639fe9c9ce6
Pragna Desu <pragna.desu@bounteous.com>
2026-02-26 12:51:25 +0530

    bash learning

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Hello from script"
+echo "Another line added"
+
+name="Pragna"
+age=22
+
+echo "My name is $name"
+echo "My age is $age"
+
+
---
+++ second-folder/script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Hello World"
+name="John"
+echo "My name is $name"
